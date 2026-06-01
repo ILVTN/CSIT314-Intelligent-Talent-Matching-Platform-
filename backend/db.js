62335603596045729a09ae37546c2486318c4b8e
Tien Dat Bui <buitiendatt25@gmail.com>
2026-06-01 20:56:25 +1000

    fix jobjs

--- backend/db.js
+++ backend/db.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "",
     database: process.env.DB_DATABASE || "talent_platform",
-    port: process.env.DB_PORT || 3307,
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10
 });
